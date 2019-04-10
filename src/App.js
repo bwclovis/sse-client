43b1dc3f5be8c6f1285c64b355ebefcf5794541d
Brian Clovis <bwclovis@outlook.com>
2019-04-10 07:33:42 -0400

    client comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    status: null
+  }
+
+  handleOnClick(evt) {
+    const eventSource = new EventSource('http://localhost:2112/api/localize');
+    console.log('[PORT OPEN FROM CLIENT]')
+    
+    eventSource.onmessage = (evt) => {
+      this.setState({
+        status: evt.data
+      })
+
+      console.log(evt)
+
+      if (evt.data === "CLOSE PORT") {
+        eventSource.close();
+        console.log('[PORT CLOSED FROM CLIENT]')
+      }
+    }
+  }
+    
+  renderStatus() {
+    return (
+      <p className="status-text">{this.state.status}</p>
+    )
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+      {this.renderStatus()}
+        <div className="selector">
+          <button className="btn" onClick={(evt) => this.handleOnClick(evt)}>Localize me</button>
+        </div>
       </div>
     );
   }
